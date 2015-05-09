ffb997ded44d39b3b3fe1d3221aa79ada78103cf
aguy867 <aguy867@gmail.com>
2015-05-09 11:35:23 -0400

    Changes

--- src/net/genesishub/gFeaturesSponge/Listeners.java
+++ src/net/genesishub/gFeaturesSponge/Listeners.java
@@ -1,20 +1,15 @@
 package net.genesishub.gFeaturesSponge;
 
-import net.genesishub.gFeaturesSponge.commands.Broadcast;
 import net.genesishub.gFeaturesSponge.commands.Speed;
 
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
 import org.spongepowered.api.event.Subscribe;
 import org.spongepowered.api.event.state.InitializationEvent;
-import org.spongepowered.api.event.state.ServerStartedEvent;
-import org.spongepowered.api.event.state.ServerStartingEvent;
+import org.spongepowered.api.event.state.PreInitializationEvent;
 import org.spongepowered.api.event.state.ServerStoppingEvent;
 import org.spongepowered.api.plugin.Plugin;
-import org.spongepowered.api.service.command.CommandService;
-import org.spongepowered.api.text.Texts;
-import org.spongepowered.api.util.command.spec.CommandSpec;
-import org.spongepowered.common.Sponge;
+import org.spongepowered.api.text.format.TextColors;
 
 import com.google.inject.Inject;
 
@@ -26,17 +21,12 @@ public class Listeners {
     @Inject
 	Logger logger;
 	@Subscribe
-	public void onServerStart(ServerStartingEvent event) {
-	    logger.info("\n______________________________________________________________________");
-	    logger.info("\n[gFeatures]Starting gFeatures-Sponge...");
-	    logger.info("\n[gFeatures]You are running gFeatures-Sponge version " + pluginversion);
-	    logger.info("\n[gFeatures]Remember! This plugin is still in Beta! Take backups!");
-	    logger.info("\n______________________________________________________________________");
-	    CommandSpec myCommandSpec = CommandSpec.builder()
-	    .setDescription(Texts.of("Sets your speed."))
-	    .setPermission("gfeatures.commands.speed")
-	    .setExecutor(new Speed())
-	    .build();
+	public void onServerStart(PreInitializationEvent event) {
+	    logger.info(TextColors.DARK_AQUA + "\n______________________________________________________________________");
+	    logger.info(TextColors.DARK_AQUA + "\n[gFeatures]Starting gFeatures-Sponge...");
+	    logger.info(TextColors.DARK_AQUA + "\n[gFeatures]You are running gFeatures-Sponge version " + pluginversion);
+	    logger.info(TextColors.DARK_AQUA + "\n[gFeatures]Remember! This plugin is still in Beta! Take backups!");
+	    logger.info(TextColors.DARK_AQUA + "\n______________________________________________________________________");
 	}
 	@Subscribe
 	public void onServerStop(ServerStoppingEvent event) {
@@ -47,15 +37,11 @@ public class Listeners {
 	    logger.info("\n______________________________________________________________________");
 	}
 	@Subscribe
-	public void Initalization(InitializationEvent event) {
-		CommandService cmdService = game.getCommandDispatcher();
-		CommandSpec speed = CommandSpec.builder()
-			    .setDescription(Texts.of("Sets your speed."))
-			    .setPermission("gfeatures.commands.speed")
-			    .setExecutor(new Speed()).build();
-				try{
-			    cmdService.register(this, speed, "speed");
-			    }
-				catch(Exception e){}
+	public void Initialization(InitializationEvent event) {
+		try{
+		game.getCommandDispatcher().register(this, new Speed(logger, game), "speed");
+		}
+		catch(Exception e){
+		}
 	}
 }
--- src/net/genesishub/gFeaturesSponge/commands/Speed.java
+++ src/net/genesishub/gFeaturesSponge/commands/Speed.java
@@ -1,7 +1,16 @@
 package net.genesishub.gFeaturesSponge.commands;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.spongepowered.api.Game;
 import org.spongepowered.api.entity.player.Player;
+import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.util.command.CommandCallable;
 import org.spongepowered.api.util.command.CommandException;
 import org.spongepowered.api.util.command.CommandResult;
 import org.spongepowered.api.util.command.CommandSource;
@@ -10,21 +19,72 @@ import org.spongepowered.api.util.command.source.CommandBlockSource;
 import org.spongepowered.api.util.command.source.ConsoleSource;
 import org.spongepowered.api.util.command.spec.CommandExecutor;
 
-public class Speed implements CommandExecutor{
+import com.google.common.base.Optional;
+
+public class Speed implements CommandCallable{
+	
+	private static Logger log;
+	private static Game game;
+	
+	public Speed(Logger logger, Game games){
+		log = logger;
+		game = games;
+	}
 
 	@Override
-	public CommandResult execute(CommandSource src, CommandContext con) throws CommandException {
-	     if(src instanceof Player) {
-	    	    Player player = (Player) src;
-	    	    player.sendMessage(Texts.of("S"));
-	    	}
-	    	else if(src instanceof ConsoleSource) {
-	    	    src.sendMessage(Texts.of(""));
-	    	}
-	    	else if(src instanceof CommandBlockSource) {
-	    	    src.sendMessage(Texts.of("Hello Companion Cube!"));
-	    	}
-	     return CommandResult.success();
+	public Optional<Text> getHelp(CommandSource arg0) {
+		Optional<Text> text = Optional.of(Texts.of(TextColors.GOLD + "/speed [amount] [player (OPTIONAL)]"));
+		return text;
+	}
+
+	@Override
+	public Optional<Text> getShortDescription(CommandSource arg0) {
+		Optional<Text> text = Optional.of(Texts.of("Sets player speed."));
+		return text;
+	}
+
+	@Override
+	public List<String> getSuggestions(CommandSource arg0, String arg1) throws CommandException {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public Text getUsage(CommandSource arg0) {
+		Text text = Texts.of(TextColors.GOLD + "/speed [amount] [player (OPTIONAL)]");
+		return text;
+	}
+
+	@Override
+	public Optional<CommandResult> process(CommandSource src, String arg1) throws CommandException {
+		if(arg1.length() > 1){
+			if(src instanceof Player){
+			src.sendMessage(Texts.of(TextColors.GOLD + "Usage: /speed [amount] [player (OPTIONAL)]"));
+			}
+			else{
+				src.sendMessage(Texts.of(TextColors.GOLD + "Usage: /speed [amount] [player]"));
+			}
+		} 
+		else{
+		if(arg1.length() == 1){
+		if(src instanceof Player) {
+			
+    	    Player player = (Player) src;
+    	    player.sendMessage(Texts.of("Speed"));
+    	}
+    	else if(src instanceof ConsoleSource) {
+    	    src.sendMessage(Texts.of(""));
+    	}
+    	else if(src instanceof CommandBlockSource) {
+    	    src.sendMessage(Texts.of("Hello Companion Cube!"));
+    	}
+		}
+		}
+		return null;
+	}
+
+	@Override
+	public boolean testPermission(CommandSource arg0) {
+		return arg0.hasPermission("gfeatures.commands.speed");
 	}
 
 }
